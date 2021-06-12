3c781948fd3506e8285bcd3cdbadc758687384c0
JazmyneB <bradleyj1@email.arizona.edu>
2021-06-12 13:08:24 -0700

    ability to click on history button and display content

--- weather/assets/js/script.js
+++ weather/assets/js/script.js
@@ -62,7 +62,7 @@ var displayCurrent = function(name, lat, long){
         //console.log(response.json())
          if (response.ok){
              response.json().then(function(data){
-                console.log(data);
+                //console.log(data);
                 //var dayOne = moment(data.daily[1].dt);
                 //var dayPlus = dayOne.format("l");
                 //console.log(dayPlus);
@@ -70,7 +70,17 @@ var displayCurrent = function(name, lat, long){
                 if (data.current.uvi === 0){
                     uv = "0";
                 }
+                if (uv >= 0 && uv < 3){
                 currentStatus.innerHTML ="<div class = 'row'> Temp: " + data.current.temp +"\xB0F </div> <br>  <div class = 'row'> Wind: " + data.current.wind_speed + " MPH </div> <br> <div class='row'> Humidity: " + data.current.humidity + " % </div> <br><div class='row'> UV Index: <span class='badge badge-success'> "+ uv + "<span></div>";
+                } else if (uv >=3 && uv <=5){
+
+                    currentStatus.innerHTML ="<div class = 'row'> Temp: " + data.current.temp +"\xB0F </div> <br>  <div class = 'row'> Wind: " + data.current.wind_speed + " MPH </div> <br> <div class='row'> Humidity: " + data.current.humidity + " % </div> <br><div class='row'> UV Index: <span class='badge badge-warning'> "+ uv + "<span></div>";
+
+                } else if (uv >=6 && uv <=10){
+                    currentStatus.innerHTML ="<div class = 'row'> Temp: " + data.current.temp +"\xB0F </div> <br>  <div class = 'row'> Wind: " + data.current.wind_speed + " MPH </div> <br> <div class='row'> Humidity: " + data.current.humidity + " % </div> <br><div class='row'> UV Index: <span class='badge badge-danger'> "+ uv + "<span></div>";
+                } else{
+                    currentStatus.innerHTML ="<div class = 'row'> Temp: " + data.current.temp +"\xB0F </div> <br>  <div class = 'row'> Wind: " + data.current.wind_speed + " MPH </div> <br> <div class='row'> Humidity: " + data.current.humidity + " % </div> <br><div class='row'> UV Index: <span class='badge badge-dark'> "+ uv + "<span></div>";
+                }
                 //forecast(name);
                 //console.log(currentDate.add(1, 'd').format('l'));
                 for (var i=0; i < 5; i++){
@@ -78,7 +88,7 @@ var displayCurrent = function(name, lat, long){
                     var dailyEl = document.getElementById("Day"+i);
                     dailyEl.innerHTML = "<h5 class='font-weight-bold'>" + currentDate.add(k, 'd').format('l')  + "</h5> <br> <div class='row'> Temp: " + data.daily[i].temp.day + "\xB0F</div><div  class='row'> Wind: " + data.daily[i].wind_speed + " MPH</div><div class ='row'> Humidity: " + data.daily[i].humidity + "%</div>"; 
                 }
-                displayHistory(name);
+                historyLog(name);
 
 
                 
@@ -87,17 +97,49 @@ var displayCurrent = function(name, lat, long){
         })
 };
 
-var displayHistory = function(name){
+var historyLog = function(name){
 
     localStorage.setItem(count, name);
-    console.log(localStorage.getItem(count));
-    historyEl.innerHTML += "<button id='histBtn' type='button' class=' btn btn-secondary btn-block'>" + localStorage.getItem(count) + "</button>";
+    //console.log(localStorage.getItem(count));
+    historyEl.innerHTML += "<button id='histBtn"+count+"' type='button' class=' btn btn-secondary btn-block' value='" + localStorage.getItem(count) + "'>" + localStorage.getItem(count) +"</button>";
 
-    var buttonEl = document.querySelector("#histBtn");
-    buttonEl.addEventListener('click', displayCurrent);
+    //var buttonEl = document.querySelector("#histBtn");
+    //buttonEl.addEventListener('click', displayHistory);
+
+    for (var i=0; i <= count; i++){
+        var buttonEl = document.querySelector('#histBtn'+i);
+        
+        buttonEl.addEventListener('click', displayHistory);
+    }
 
 
     count++;
+    
+}
+
+var displayHistory = function(event){
+
+    event.preventDefault();
+
+    //console.log(event.target.value);
+    var city = event.target.value;
+
+    fetch(apiUrl + city + "&units=imperial&appid=55d754b5ee491bdb9f01f7162bd9bbd5").then(function(response){
+        //console.log(response.json()); //Getting the promise
+        //Need Name: Date to display
+        //Need Temp, Wind, Humidity & UV Index
+        if (response.ok) {
+             response.json().then(function(data) {
+                //historyEl.innerHTML
+               displayCurrent(city, data.coord.lat, data.coord.lon);
+               //displayHistory(cityEl.value);
+             });
+           } else {
+             alert("Error: Please check spelling");
+           }
+    
+    });
+
     
 }
 
